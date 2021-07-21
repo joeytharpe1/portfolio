22e33d9eb8100fbbc24e74da06428fba9840c51d
Joseph Tharpe <solowun6@aol.com>
2021-07-21 11:14:44 -0700

    added wine project

--- src/Shared/project.js
+++ src/Shared/project.js
@@ -2,20 +2,19 @@
 export const PROJECTS = [
     {
         id: 1,
+        name: "Winery",
+        image: "images/wineproject.png",
+        description: "Wine website built with bootsrap and html5",
+        url: 'https://jtharpe-winery.netlify.app',
+        button: 'Relax and unwine'
+    },
+    {
+        id: 2,
         name: "Star Wars App",
         image: "images/starwarsScreen.png",
         description: "Star Wars Info site with a movie API",
         url: 'http://jtharpe-starwars.netlify.app',
         button: 'Feel the Force'
-    },
-
-    {
-        id: 2,
-        name: "Weather App",
-        image: "images/weather.png",
-        description: "Dynamic weather app 🌴",
-        url: 'https://jtharpe-weather-app.netlify.app',
-        button: 'experience the weather in action'
     },
     {
         id: 3,
@@ -27,6 +26,14 @@ export const PROJECTS = [
     },
     {
         id: 4,
+        name: "Weather App",
+        image: "images/weather.png",
+        description: "Dynamic weather app 🌴",
+        url: 'https://jtharpe-weather-app.netlify.app',
+        button: 'experience the weather in action'
+    },
+    {
+        id: 5,
         name: "Coffee Clone",
         image: "images/coffee.png",
         description: "starbucks coffee clone ☕",
@@ -34,20 +41,30 @@ export const PROJECTS = [
         button: 'hmm craving coffee ?'
     },
     {
-        id: 5,
+        id: 6,
         name: "To-do Cooking Recipe",
         image: 'images/toodo.png',
         description: "todo list for storing ingredients 👨‍🍳",
         url: 'https://jtharpe-toodo-list.netlify.app',
         button: 'see how it works'
     },
-
-
     // {
     //     id: 6,
-    //     name: "project 4",
-    //     image: "images/starwars.jpg",
-    //     description: "project 4 description",
-    //     url: ''
-    // }
+    //     name: "To-do Cooking Recipe",
+    //     image: 'images/toodo.png',
+    //     description: "todo list for storing ingredients 👨‍🍳",
+    //     url: 'https://jtharpe-toodo-list.netlify.app',
+    //     button: 'see how it works'
+    // },
+    // {
+    //     id: 6,
+    //     name: "To-do Cooking Recipe",
+    //     image: 'images/toodo.png',
+    //     description: "todo list for storing ingredients 👨‍🍳",
+    //     url: 'https://jtharpe-toodo-list.netlify.app',
+    //     button: 'see how it works'
+    // },
+
+
+  
 ];
